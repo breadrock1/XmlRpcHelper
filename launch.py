5c2cfe45d005bb345a9f4d407844001d22db83ac
Bread White <breadrock1@gmail.com>
2021-11-06 21:36:10 +0300

    Has been fixed LICENSE documentation into launch.py file

--- launch.py
+++ launch.py
@@ -1,21 +1,29 @@
 #!/usr/bin/python3
+
 """
-Copyright (C) 2021  Bread White
+    Copyright (c) 2021 Bread White
 
-This program is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
+    Permission is hereby granted, free of charge, to any person obtaining a copy
+    of this software and associated documentation files (the "Software"), to deal
+    in the Software without restriction, including without limitation the rights
+    to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+    copies of the Software, and to permit persons to whom the Software is
+    furnished to do so, subject to the following conditions:
 
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
+    The above copyright notice and this permission notice shall be included in all
+    copies or substantial portions of the Software.
 
-Author: Bread White
-Date:   2021 year
-Email: breadrock1@gmail.com
+    THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+    IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+    FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+    AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+    LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+    OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+    SOFTWARE.
 
+    Author: Bread White
+    Date:   2021 year
+    Email: breadrock1@gmail.com
 """
 
 from argparse import ArgumentParser
@@ -24,8 +32,6 @@ from src.XmlRpcHelper import XmlRpcHelper
 
 
 def main() -> None:
-    mode = ''
-
     argumentParser = ArgumentParser(
         prog='XmlRpcHelper',
         usage='''./XmlPrcHelper.py MODE {AllMethods | Ping | Bruting} -u URL
@@ -43,6 +49,8 @@ def main() -> None:
         allow_abbrev=True
     )
 
+    mode = ''
+
     subArgumentParser = argumentParser.add_subparsers(title='Script Mods', dest=mode)
     ping = subArgumentParser.add_parser('pingback.ping', help='ping back feature')
     brute = subArgumentParser.add_parser('wp.getUsersBlogs', help='brute force credentials')
@@ -54,9 +62,9 @@ def main() -> None:
     ping.add_argument('-d', type=str, required=True, metavar='--dst', help='Specify destination address (it\'s target server)')
     ping.add_argument('-u', type=str, required=True, metavar='--url', help='Specify url address')
 
+    brute.add_argument('-u', type=str, required=True, metavar='--url', help='Specify url address')
     brute.add_argument('-l', type=str, required=False, metavar='--login', help='Specify login')
     brute.add_argument('-p', type=str, required=False, metavar='--pass-wordlist', help='Specify path to wordlist')
-    brute.add_argument('-u', type=str, required=True, metavar='--url', help='Specify url address')
 
     ping.set_defaults(mode='pingback.ping')
     brute.set_defaults(mode='wp.getUsersBlogs')
